a833a43596a2a14f7a4e23c7322c3ade34272839
JAIRO-99 <jairo.lcelis1999@gmail.com>
2024-10-21 12:10:25 -0500

    Initial Commit

---
+++ WheatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WheatherApp
+//
+//  Created by Jairo Laurente Celis on 21/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WheatherApp/WheatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheatherAppApp.swift
+//  WheatherApp
+//
+//  Created by Jairo Laurente Celis on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WheatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
